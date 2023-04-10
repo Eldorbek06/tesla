be6130cd7365ffca0e17fedc2bb7125b491b548d
Eldor06 <eldorergashev20sm>
2023-04-10 18:52:27 +0500

    balon rotation

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 let switch_btns = document.querySelectorAll('.switch')
+let power_reserve = document.querySelector('.power_reserve h1')
+let price = document.querySelector('.price h1')
+let speed = document.querySelector('.speed')
+let temperature = document.querySelector('.speed')
+let disks = document.querySelector('.speed')
 
-switch_btns.forEach(el => {
-    el.onclick = () => {
-        el.classList.toggle('on')
-    }
-})
+
+switch_btns.forEach(el => el.onclick = () => el.classList.toggle('on'))
